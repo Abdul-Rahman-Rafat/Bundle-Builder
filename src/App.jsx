5bd1637c09db71d1555b6644b0843484f2843f61
Abdul-Rahman-Rafat <abdorafat489@gmail.com>
2026-07-26 18:13:49 +0300

    add "Gilroy" Font

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,39 @@
 import "./App.css";
+import BundleBuilder from "./pages/BundleBuilder";
+
+//context imports
+// import { useContext } from "react";
+// import { ProductsContext } from "./context/ProductsContext";
+// import { StepsContext } from "./context/StepsContext";
 
 function App() {
+  ////useContexts
+  // const { products } = useContext(ProductsContext);
+  // const { steps } = useContext(StepsContext);
   return (
-    <>
-      <h1 className="text-amber-300">Welcome</h1>
-    </>
+    <div style={{ fontFamily: "Gilroy" }}>
+      <BundleBuilder />
+
+      {/* context Data */}
+      {/* <div className="productsdata">
+        {products
+          .filter((product) => {
+            return product.id <= 6;
+          })
+          .map((p) => (
+            <h3 key={p.id}>{p.name}</h3>
+          ))}
+      </div>
+      <div className="stepsdata">
+        {steps
+          .filter((step) => {
+            return step.id <= 6;
+          })
+          .map((p) => (
+            <h3 key={p.id}>{p.title}</h3>
+          ))}
+      </div> */}
+    </div>
   );
 }
 
